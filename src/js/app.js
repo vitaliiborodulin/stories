5dd7f3123e8a487a57fc5d5bf8a79bfd04c0b1ce
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2020-03-11 09:14:42 +0300

    add jquery? remove unnecessary

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,12 @@
-//= ../../node_modules/blazy/blazy.js
+//= ../../node_modules/jquery/dist/jquery.min.js
 //= ../../node_modules/inputmask/dist/jquery.inputmask.min.js
 //= ../../node_modules/imagelightbox/dist/imagelightbox.min.js
 
 $(function(){
 
-    //= components/blazy.js
     //= components/inputmask.js
-    //= components/popup.js
     //= components/header-menu.js
     //= components/imagelightbox.js
-    //= components/footer-arrow.js
 
     console.log(1);
 
