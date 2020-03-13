b808a15f1ffbe8c13f83738d4f1782cb8fac1861
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2020-03-13 08:44:41 +0300

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 //= ../../node_modules/jquery/dist/jquery.min.js
 //= ../../node_modules/inputmask/dist/jquery.inputmask.min.js
 //= ../../node_modules/imagelightbox/dist/imagelightbox.min.js
-//= ../../node_modules/slick-carousel/slick/slick.min.js
 
 $(function(){
 
